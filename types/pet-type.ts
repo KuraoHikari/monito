97e0dd4839124c1d348cac18987b6802df60ba57
KuraoHikari <dewaindra705@gmail.com>
2024-03-18 16:43:57 +0800

    update type card

--- types/pet-type.ts
+++ types/pet-type.ts
@@ -1,10 +1,14 @@
 export type Gene = "Male" | "Female";
 
+export type Product = "Dog Food" | "Cat Food" | "Toy" | "Costume";
+
 export type PetProductType = {
  id: string;
  title: string;
- gene: Gene;
- age: number;
+ gene?: Gene;
+ age?: number;
+ product?: Product;
+ size?: number;
  price: number;
  image: string;
 };
