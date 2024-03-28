7b18247cb22602e6d8541240782046ae8523136b
KuraoHikari <dewaindra705@gmail.com>
2024-03-28 11:23:43 +0800

    chore: maintance HeroBanner tailwind feat : add tailwind auto format prettier, and add new dev depedency

--- components/Banner/HeroBanner.tsx
+++ components/Banner/HeroBanner.tsx
@@ -1,270 +1,261 @@
-import { PlayCircle } from "lucide-react";
-import { Button } from "../ui/button";
-import Image from "next/image";
+import { PlayCircle } from 'lucide-react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
 
 export const HeroBanner = () => {
- return (
-  <div
-   className="
-        overflow-hidden
-        relative 
-        z-20 
-        bg-[linear-gradient(102.87deg,#FCEED5_6.43%,#FCEED5_78.33%,#FFE7BA_104.24%)] 
-        lg:h-[695px]
-        md:h-[1088px]
-      
-        h-[988px]
-        rounded-b-[50px]
-        "
-  >
-   <div className="">
-    {/* Hero Text */}
+  return (
     <div
-     className="
-        lg:w-1/3
-        lg:ml-[130px] 
-        md:mx-24
-        mx-8
-
-        pt-[180px] 
-        lg:pt-[100px]
-        xl:pt-[180px]
-        
-
-        
-  "
+      className="
+        relative
+        z-20 
+        h-[988px] 
+        overflow-hidden 
+        rounded-b-[50px]
+        bg-[linear-gradient(102.87deg,#FCEED5_6.43%,#FCEED5_78.33%,#FFE7BA_104.24%)]
+      
+        md:h-[1088px]
+        lg:h-[695px]"
     >
-     {/* Hero Title */}
-     <div>
-      <h1
-       className="
-      z-10
-      text-dark-blue-80
-      text-[60px] 
-      leading-[68px]
-      font-bold
-      "
-      >
-       One more friend
-      </h1>
-     </div>
-     {/* Hero SubTitle */}
-     <div>
-      <h2
-       className="
-       text-dark-blue-80
-    
-       text-[50px] 
-       leading-[60px]"
-      >
-       Thousands more fun!
-      </h2>
-     </div>
-     {/* Hero Desc */}
-     <div className="pl-[0.5rem] my-4">
-      <p
-       className="
-       lg:max-w-[380px] 
-       xl:max-w-[480px] 
-       text-[#242b33] 
-       font-medium 
-       text-[16px] 
-       leading-[24px] "
-      >
-       Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We
-       have 200+ different pets that can meet your needs!
-      </p>
-     </div>
-     {/* Hero Buton Section */}
-     <div className="ml-2 mt-7 z-10">
-      <Button
-       size="lg"
-       variant="outline"
-       className="
+      <div className="">
+        {/* Hero Text */}
+        <div
+          className="
+            mx-8
+            pt-[180px] 
+            md:mx-24
+            lg:ml-[130px]
+
+            lg:w-1/3 
+            lg:pt-[100px]
+            xl:pt-[180px]"
+        >
+          {/* Hero Title */}
+          <div>
+            <h1
+              className="
+                z-10
+                text-[60px]
+                font-bold 
+                leading-[68px]
+                text-dark-blue-80"
+            >
+              One more friend
+            </h1>
+          </div>
+          {/* Hero SubTitle */}
+          <div>
+            <h2
+              className="
+                  text-[50px]
+                
+                  leading-[60px] 
+                  text-dark-blue-80"
+            >
+              Thousands more fun!
+            </h2>
+          </div>
+          {/* Hero Desc */}
+          <div className="my-4 pl-[0.5rem]">
+            <p
+              className="
+                text-[16px] 
+                font-medium 
+                leading-[24px] 
+                text-neutral-80 
+                lg:max-w-[380px] 
+                xl:max-w-[480px]"
+            >
+              Having a pet means you have more joy, a new friend, a happy person
+              who will always be with you to have fun. We have 200+ different
+              pets that can meet your needs!
+            </p>
+          </div>
+          {/* Hero Buton Section */}
+          <div className="z-10 ml-2 mt-7">
+            <Button
+              size="lg"
+              variant="outline"
+              className="
                 rounded-full 
-                text-dark-blue-80
-                sm:text-[1rem] 
-                text-[12px] 
+                border-2
                 border-dark-blue-80 
-                hover:bg-dark-blue-40 
-                hover:text-white
-                hover:border-none
-                border-2 
-                lg:py-[10px] 
-                lg:px-[28px]
-       "
-      >
-       View Intro <PlayCircle className="ml-2 h-4 w-4" />
-      </Button>
-      <Button
-       variant="default"
-       className="
+                text-[12px] 
+                text-dark-blue-80 
+
+                hover:border-none 
+                hover:bg-dark-blue-40
+                hover:text-neutral-00
+
+                sm:text-[1rem] 
+                lg:px-[28px] 
+                lg:py-[10px]"
+            >
+              View Intro <PlayCircle className="ml-2 h-4 w-4" />
+            </Button>
+            <Button
+              variant="default"
+              className="
                 ml-3 
                 rounded-full 
-                text-gray-200 
-                sm:text-[1rem]
-                text-[12px] 
+
                 bg-dark-blue-80
-                 
+
+                text-[12px]
+                text-neutral-00 
                 hover:bg-dark-blue-40
-                lg:py-[10px] 
-                lg:px-[28px]
+                 
+                sm:text-[1rem]
+                lg:px-[28px] 
+                lg:py-[10px]
        "
-      >
-       Explore Now
-      </Button>
-     </div>
+            >
+              Explore Now
+            </Button>
+          </div>
+        </div>
+        {/* Hero Image */}
+        <Image
+          src="/banner/goodHumoredWomanHoldsDogLaughi.png"
+          alt=""
+          width={944}
+          height={693}
+          className="
+              absolute 
+              bottom-0 
+
+              z-20 
+              
+              lg:left-[330px] 
+              lg:top-[84px] 
+              xl:left-[730px]"
+        />
+        <div
+          className="
+            absolute 
+            bottom-0 left-32
+            z-10  
+            h-[335px]  
+            w-[335px]
+            rounded-[99px]
+
+            bg-dark-blue 
+            [transform:matrix(0.9867,_0.1625,_-0.1625,_0.9867,_0,_0)] 
+
+            md:left-80 
+            md:h-[435px] 
+            md:w-[435px] 
+            lg:left-[500px] 
+            lg:top-[268px]
+            lg:h-[635px]
+
+            lg:w-[635px] 
+            xl:left-[900px]"
+        />
+        <div
+          className="
+            bg-moon-yellow 
+            absolute 
+            bottom-0
+            left-52
+            z-10
+            h-[335px]  
+            w-[335px] 
+
+            origin-top-left
+            rounded-[99px]
+            [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)] 
+
+            md:left-80 
+            md:h-[435px] 
+            md:w-[435px] 
+            lg:left-[665px] 
+            lg:top-[180px] 
+            lg:h-[635px] 
+            lg:w-[635px] 
+            xl:left-[1165px]"
+        />
+        <div
+          className="
+            bg-moon-yellow 
+            absolute 
+            left-[438.334px] 
+            top-[564px] 
+            h-[635px] 
+            w-[635px] 
+            origin-top-left 
+            rounded-[99px]
+            opacity-40 
+            [transform:matrix(0.5524,_0.8336,_-0.8336,_0.5524,_0,_0)]"
+        />
+
+        <div
+          className="
+            bg-moon-yellow 
+            absolute 
+            -left-[64.3297px] 
+            -top-[697px] 
+            h-[635px] 
+            w-[635px] 
+            origin-top-left 
+            rounded-[99px]
+            [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)]"
+        />
+
+        <div
+          className="
+            bg-moon-yellow
+            absolute 
+            left-[140.6029px] 
+            top-[163px] 
+            -z-10 
+            h-[67.1033px] 
+            w-[67.1033px] 
+            origin-top-left 
+            rounded-[20px]
+            [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)]"
+        />
+        <div
+          className="
+            bg-moon-yellow
+            absolute 
+            left-[762.192px] 
+            top-[148px] 
+            -z-10 
+            h-[14.6264px] 
+            w-[14.6264px] 
+            origin-top-left 
+            rounded-[4px]
+            [transform:matrix(0.9349,_0.355,_-0.355,_0.9349,_0,_0)]"
+        />
+
+        <div
+          className="
+          bg-moon-yellow
+          absolute 
+          left-[728px] top-[210.678px] 
+          -z-10 
+          h-[27.4976px] 
+          w-[27.4976px] 
+          origin-top-left 
+          rounded-[9px]
+          [transform:matrix(0.9215,_-0.3883,_0.3883,_0.9215,_0,_0)]
+    "
+        />
+
+        <div
+          className="
+          absolute
+          left-[727px] 
+          top-[223.6443px] 
+          -z-10 
+          h-[21.4707px] 
+          w-[21.4707px] 
+          origin-top-left 
+          rounded-[6px] 
+          bg-dark-blue-80 
+          [transform:matrix(0.7313,_-0.6821,_0.6821,_0.7313,_0,_0)]"
+        />
+      </div>
     </div>
-    {/* Hero Image */}
-    <Image
-     src="/banner/goodHumoredWomanHoldsDogLaughi.png"
-     alt=""
-     width={944}
-     height={693}
-     className="
-        z-20 
-        absolute 
-
-        xl:left-[730px] 
-        lg:top-[84px] 
-        lg:left-[330px] 
-        bottom-0
-     "
-    />
-    <div
-     className="
-        z-10 
-
-        md:left-80
-        lg:top-[268px]
-        xl:left-[900px]  
-        lg:left-[500px]  
-
-        bottom-0
-        left-32
-
-        absolute 
-        lg:w-[635px] 
-        lg:h-[635px] 
-        md:w-[435px] 
-        md:h-[435px] 
-        w-[335px] 
-        h-[335px] 
-        rounded-[99px] 
-        bg-dark-blue
-        [transform:matrix(0.9867,_0.1625,_-0.1625,_0.9867,_0,_0)]
-     "
-    />
-    <div
-     className="
-        z-10 
-        origin-top-left 
-        absolute
-
-        md:left-80
-        lg:top-[180px]
-        lg:left-[665px]  
-        xl:left-[1165px] 
-
-        bottom-0
-        left-52
-
-        lg:w-[635px] 
-        lg:h-[635px] 
-        md:w-[435px] 
-        md:h-[435px] 
-        w-[335px] 
-        h-[335px] 
-        rounded-[99px] 
-        bg-[#f7dba7] 
-        [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)]
-     "
-    />
-    <div
-     className="
-    opacity-40 
-    origin-top-left 
-    absolute 
-    left-[438.334px] 
-    top-[564px] 
-    w-[635px] 
-    h-[635px] 
-    rounded-[99px] 
-    bg-[#f7dba7] 
-    [transform:matrix(0.5524,_0.8336,_-0.8336,_0.5524,_0,_0)]"
-    />
-
-    <div
-     className="
-    origin-top-left 
-    absolute 
-    -left-[64.3297px] 
-    -top-[697px] 
-    w-[635px] 
-    h-[635px] 
-    rounded-[99px] 
-    bg-[#f7dba7] 
-    [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)]
-    "
-    />
-
-    <div
-     className="
-     -z-10
-    origin-top-left 
-    absolute 
-    left-[140.6029px] 
-    top-[163px] 
-    w-[67.1033px] 
-    h-[67.1033px] 
-    rounded-[20px] 
-    bg-[#f7dba7] 
-    [transform:matrix(0.9046,_0.4263,_-0.4263,_0.9046,_0,_0)]
-    "
-    />
-    <div
-     className="
-     -z-10
-    origin-top-left 
-    absolute 
-    left-[762.192px] 
-    top-[148px] 
-    w-[14.6264px] 
-    h-[14.6264px] 
-    rounded-[4px] 
-    bg-[#f7dba7] 
-    [transform:matrix(0.9349,_0.355,_-0.355,_0.9349,_0,_0)]
-    "
-    />
-
-    <div
-     className="
-     -z-10
-     origin-top-left 
-     absolute left-[728px] 
-     top-[210.678px] 
-     w-[27.4976px] 
-     h-[27.4976px] 
-     rounded-[9px] 
-     bg-[#f7dba7] 
-     [transform:matrix(0.9215,_-0.3883,_0.3883,_0.9215,_0,_0)]
-    "
-    />
-
-    <div
-     className="
-     -z-10
-    origin-top-left 
-    absolute 
-    left-[727px] 
-    top-[223.6443px] 
-    w-[21.4707px] 
-    h-[21.4707px] 
-    rounded-[6px] 
-    bg-[#002a48] 
-    [transform:matrix(0.7313,_-0.6821,_0.6821,_0.7313,_0,_0)]"
-    />
-   </div>
-  </div>
- );
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,94 +1,99 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config = {
- darkMode: ["class"],
- content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
- prefix: "",
- theme: {
-  container: {
-   center: true,
-   padding: "2rem",
-   screens: {
-    "2xl": "1400px",
-   },
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        'dark-blue': '#003459',
+        'dark-blue-40': '#0078CD',
+        'dark-blue-60': '#00528C',
+        'dark-blue-80': '#002A48',
+        'moon-yellow': '#F7DBA7',
+        'moon-yellow-40': '#FCEED5',
+        'moon-yellow-60': '#F1D092',
+        'moon-yellow-80': '#EEC77E',
+        'pink-red': '#ff564f',
+        'green-light': '#34c759',
+        'orange-shine': '#ff912c',
+        'blue-sea': '#00a7e7',
+        'neutral-100': '#00171f',
+        'neutral-80': '#242b33',
+        'neutral-60': '#667479',
+        'neutral-40': '#99a2a5',
+        'neutral-20': '#ccd1d2',
+        'neutral-10': '#ebeeef',
+        'neutral-00': '#fdfdfd',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  extend: {
-   colors: {
-    "dark-blue": "#003459",
-    "dark-blue-40": "#0078CD",
-    "dark-blue-60": "#00528C",
-    "dark-blue-80": "#002A48",
-    "mon-yellow": "#F7DBA7",
-    "moon-yellow-40": "#FCEED5",
-    "moon-yellow-60": "#F1D092",
-    "moon-yellow-80": "#EEC77E",
-    "pink-red": "#ff564f",
-    "green-light": "#34c759",
-    "orange-shine": "#ff912c",
-    "blue-sea": "#00a7e7",
-    "neutral-100": "#00171f",
-    "neutral-80": "#242b33",
-    "neutral-60": "#667479",
-    "neutral-40": "#99a2a5",
-    "neutral-20": "#ccd1d2",
-    "neutral-10": "#ebeeef",
-    "neutral-00": "#fdfdfd",
-    border: "hsl(var(--border))",
-    input: "hsl(var(--input))",
-    ring: "hsl(var(--ring))",
-    background: "hsl(var(--background))",
-    foreground: "hsl(var(--foreground))",
-    primary: {
-     DEFAULT: "hsl(var(--primary))",
-     foreground: "hsl(var(--primary-foreground))",
-    },
-    secondary: {
-     DEFAULT: "hsl(var(--secondary))",
-     foreground: "hsl(var(--secondary-foreground))",
-    },
-    destructive: {
-     DEFAULT: "hsl(var(--destructive))",
-     foreground: "hsl(var(--destructive-foreground))",
-    },
-    muted: {
-     DEFAULT: "hsl(var(--muted))",
-     foreground: "hsl(var(--muted-foreground))",
-    },
-    accent: {
-     DEFAULT: "hsl(var(--accent))",
-     foreground: "hsl(var(--accent-foreground))",
-    },
-    popover: {
-     DEFAULT: "hsl(var(--popover))",
-     foreground: "hsl(var(--popover-foreground))",
-    },
-    card: {
-     DEFAULT: "hsl(var(--card))",
-     foreground: "hsl(var(--card-foreground))",
-    },
-   },
-   borderRadius: {
-    lg: "var(--radius)",
-    md: "calc(var(--radius) - 2px)",
-    sm: "calc(var(--radius) - 4px)",
-   },
-   keyframes: {
-    "accordion-down": {
-     from: { height: "0" },
-     to: { height: "var(--radix-accordion-content-height)" },
-    },
-    "accordion-up": {
-     from: { height: "var(--radix-accordion-content-height)" },
-     to: { height: "0" },
-    },
-   },
-   animation: {
-    "accordion-down": "accordion-down 0.2s ease-out",
-    "accordion-up": "accordion-up 0.2s ease-out",
-   },
-  },
- },
- plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
