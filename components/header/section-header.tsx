6c01fd80547992617e0ba69427ee7d8250a57e85
KuraoHikari <dewaindra705@gmail.com>
2024-04-29 19:58:05 +0800

    update section header responsive for mobile

--- components/header/section-header.tsx
+++ components/header/section-header.tsx
@@ -1,42 +1,44 @@
-import { ChevronRight } from "lucide-react";
-import { Button } from "../ui/button";
+import { ChevronRight } from 'lucide-react';
+import { Button } from '../ui/button';
 
 interface SectionHeaderProps {
- buttonText?: string | null;
- title: string;
- subTitle?: string;
+  buttonText?: string | null;
+  title: string;
+  subTitle?: string;
 }
 
 const SectionHeader = ({ buttonText, title, subTitle }: SectionHeaderProps) => {
- return (
-  <div className="flex justify-between py-[28px] px-[130px]">
-   <div className="align-middle">
-    {subTitle && <h4 className="text-lg">{subTitle}</h4>}
-    <h1 className="text-[#002a48] font-bold text-2xl">{title}</h1>
-   </div>
-   <div>
-    {buttonText && (
-     <Button
-      className="mt-4
-     rounded-full 
-                text-[#003459] 
-                sm:text-[1rem] 
-                text-[12px] 
-                border-[#003459] 
-                hover:bg-[#0078CD] 
-                hover:text-white
-                hover:border-none
+  return (
+    <div className="flex justify-between px-5 py-[28px] sm:px-[130px]">
+      <div className="align-middle">
+        {subTitle && <h4 className="text-lg">{subTitle}</h4>}
+        <h1 className="text-2xl font-bold text-[#002a48]">{title}</h1>
+      </div>
+      <div>
+        {buttonText && (
+          <Button
+            className="
+                mx-4
+                mt-4 
+                rounded-full 
                 border-2 
-                lg:py-[10px] 
-                lg:px-[28px]"
-     >
-      {buttonText}
-      <ChevronRight className="ml-2 h-4 w-4" />
-     </Button>
-    )}
-   </div>
-  </div>
- );
+                border-[#003459] 
+                text-[12px] 
+                text-[#003459] 
+                hover:border-none
+                hover:bg-[#0078CD]
+                hover:text-white 
+                sm:text-[1rem]
+                lg:px-[28px] 
+                lg:py-[10px]"
+          >
+            {buttonText}
+            <ChevronRight className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SectionHeader;
